bd41069756111c9c35ab06d7ef43e853b6c05a83
Norman Chen <njchen@uwaterloo.ca>
2024-05-24 20:14:07 -0400

    init backend

---
+++ backend/app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from .config import DevelopmentConfig, ProductionConfig
+from .routes.api import api_v1 as api_v1_blueprint
+from .routes.main import main as main_blueprint
+
+
+def create_app():
+    app = Flask(__name__)
+
+    if os.getenv("FLASK_ENV") == "production":
+        app.config.from_object(ProductionConfig())
+    else:
+        app.config.from_object(DevelopmentConfig())
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(api_v1_blueprint)
+
+    return app
